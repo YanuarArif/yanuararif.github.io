9f9f65628b19e936649a73c2277ec4fe6bcb3a30
Yanuar Lukman Arif <yanuarlukmanarif@gmail.com>
2024-05-03 19:26:31 +0700

    comit pertama

---
+++ components/Layout.js
@@ -0,0 +1,27 @@
+// fonts
+import { Sora } from "@next/font/google";
+// components
+import Nav from "../components/Nav";
+import Header from "../components/Header";
+import TopLeftImg from "../components/TopLeftImg";
+
+// font settings
+const sora = Sora({
+  subsets: ["latin"],
+  variable: "--font-sora",
+  weight: ["100", "200", "300", "400", "500", "600", "700"],
+});
+
+const Layout = ({ children }) => {
+  return (
+    <div
+      className={`page bg-site text-white bg-cover ${sora.variable} font-sora relative`}>
+      <TopLeftImg />
+      <Nav />
+      <Header />
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
